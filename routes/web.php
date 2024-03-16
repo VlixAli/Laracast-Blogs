ea4acdc8f5f22d5d57cc5de4a3343ce091c107d6
Ali <qali372@gmail.com>
2024-03-16 23:44:59 +0200

    refactor : organize web.php routes file to be more readable

--- routes/web.php
+++ routes/web.php
@@ -9,27 +9,36 @@ use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [PostController::class, 'index'])->name('home');
+Route::controller(PostController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('posts/{post:slug}', 'show');
+});
 
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
 
 Route::post('newsletter', NewsletterController::class);
 
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+Route::controller(RegisterController::class)
+    ->middleware('guest')->prefix('register')->group(function () {
+        Route::get('/', 'create');
+        Route::post('/', 'store');
+    });
 
-Route::get('login', [SessionController::class, 'create'])->middleware('guest');
-Route::post('login', [SessionController::class, 'store'])->middleware('guest');
+Route::controller(SessionController::class)->group(function () {
+    Route::middleware('guest')->prefix('login')->group(function () {
+        Route::get('/', 'create');
+        Route::post('/', 'store');
+    });
 
-Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
-
-Route::middleware('can:admin')->group(function () {
-
-    Route::get('admin/posts', [AdminPostController::class, 'index'])->name('admin_posts');
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts/create', [AdminPostController::class, 'create'])->name('post');
-    Route::get('admin/posts/{post:slug}/edit', [AdminPostController::class, 'edit'])->name('edit_post');
-    Route::patch('admin/posts/{post:slug}', [AdminPostController::class, 'update'])->name('update_post');
-    Route::delete('admin/posts/{post:slug}', [AdminPostController::class, 'destroy'])->name('delete_post');
+    Route::post('logout', 'destroy')->middleware('auth');
 });
+
+Route::controller(AdminPostController::class)
+    ->middleware('can:admin')->group(function () {
+        Route::get('admin/posts', 'index')->name('admin_posts');
+        Route::post('admin/posts', 'store');
+        Route::get('admin/posts/create', 'create')->name('post');
+        Route::get('admin/posts/{post:slug}/edit', 'edit')->name('edit_post');
+        Route::patch('admin/posts/{post:slug}', 'update')->name('update_post');
+        Route::delete('admin/posts/{post:slug}', 'destroy')->name('delete_post');
+    });
